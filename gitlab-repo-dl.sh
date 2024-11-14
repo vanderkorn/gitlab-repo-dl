e82333e1481182bc27e9778833e00f170d82d3f6
Ivan Kornilov <vanderkorn@gmail.com>
2024-11-14 23:35:20 +0500

    Update gitlab-repo-dl.sh

--- gitlab-repo-dl.sh
+++ gitlab-repo-dl.sh
@@ -11,6 +11,11 @@ if [ -z "$GITLAB_TOKEN" ]; then
     exit 1
 fi
 
+if [ -z "$GITLAB_PROTOCOL" ]; then
+    echo "Missing environment variable: GITLAB_PROTOCOL"
+    exit 1
+fi
+
 if [ -z "$1" ]; then
     echo "Action is required. Can be one of 'group', 'all-repo-list', 'from-list'"
     exit 1
@@ -26,12 +31,19 @@ if [ "$1" == "group" ]; then
 
     echo "Cloning all git projects in group $GROUP_NAME"
     
-    TOTAL_PAGES=`curl "$GITLAB_URL/api/v4/groups/$GROUP_NAME/projects?private_token=$GITLAB_TOKEN&per_page=100" -sI | grep x-total-pages | awk '{print $2}' | sed 's/\\r//g'`
+    TOTAL_PAGES=`curl "$GITLAB_URL/api/v4/groups/$GROUP_NAME/projects?private_token=$GITLAB_TOKEN&per_page=100" -sI | grep -e x-total-pages -e X-Total-Pages | awk '{print $2}' | sed 's/\\r//g'`
 
     for ((PAGE_NUMBER = 1; PAGE_NUMBER <= TOTAL_PAGES; PAGE_NUMBER++)); do
-        REPO_SSH_URLS=$(curl -s "$GITLAB_URL/api/v4/groups/$GROUP_NAME/projects?private_token=$GITLAB_TOKEN&per_page=100&page=$PAGE_NUMBER" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g')
 
-        for REPO_SSH_URL in $REPO_SSH_URLS; do
+		if [[ "$GITLAB_PROTOCOL" == "https" ]]; then
+			echo "Switch to to https protocol"
+			REPO_SSH_URLS=$(curl -s "$GITLAB_URL/api/v4/groups/$GROUP_NAME/projects?private_token=$GITLAB_TOKEN&per_page=100&page=$PAGE_NUMBER" | jq '.[] | .http_url_to_repo' | sed 's/"//g' | sed 's/http:/https:/')
+		else
+			echo "Switch to to ssh protocol"
+		    REPO_SSH_URLS=$(curl -s "$GITLAB_URL/api/v4/groups/$GROUP_NAME/projects?private_token=$GITLAB_TOKEN&per_page=100&page=$PAGE_NUMBER" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g')
+		fi
+
+		for REPO_SSH_URL in $REPO_SSH_URLS; do
             REPO_PATH="$GROUP_NAME/$(echo "$REPO_SSH_URL" | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}')"
 
             if [ ! -d "$REPO_PATH" ]; then
@@ -47,11 +59,16 @@ if [ "$1" == "group" ]; then
     
 elif [ "$1" == "all-repo-list" ]; then
     # Get total number of pages (with 20 projects per page) from HTTP header
-    TOTAL_PAGES=`curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&membership=true" -sI | grep x-total-pages | awk '{print $2}' | sed 's/\\r//g'`
+    TOTAL_PAGES=`curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&membership=true" -sI | grep -e x-total-pages -e X-Total-Pages | awk '{print $2}' | sed 's/\\r//g'`
      
     for ((PAGE_NUMBER = 1; PAGE_NUMBER <= TOTAL_PAGES; PAGE_NUMBER++)); do
-        # echo git@instance:namespace/repo.git
-        curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&per_page=20&page=$PAGE_NUMBER&membership=true" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g'
+		if [[ "$GITLAB_PROTOCOL" == "https" ]]; then
+			echo "Switch to to https protocol"
+			curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&per_page=20&page=$PAGE_NUMBER&membership=true" | jq '.[] | .http_url_to_repo' | sed 's/"//g' |  sed 's/http:/https:/'
+		else
+			echo "Switch to to ssh protocol"
+			curl "$GITLAB_URL/api/v4/projects?private_token=$GITLAB_TOKEN&per_page=20&page=$PAGE_NUMBER&membership=true" | jq '.[] | .ssh_url_to_repo' | sed 's/"//g' |  sed 's/http:/https:/'
+		fi
     done
 elif [ "$1" == "from-list" ]; then
     if [ -z "$2" ]; then
